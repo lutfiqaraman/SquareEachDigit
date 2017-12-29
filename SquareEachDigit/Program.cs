8bd4b89d3c8a82f3fdb416c9dbbf01d132d0e54d
lutfiqaraman <lutfiqaraman@gmail.com>
2017-12-29 23:58:44 +0200

    Done

--- SquareEachDigit/Program.cs
+++ SquareEachDigit/Program.cs
@@ -10,6 +10,25 @@ namespace SquareEachDigit
     {
         static void Main(string[] args)
         {
+            int n = 12345;
+            Console.WriteLine(SquareEachDigit(n));
+            Console.ReadLine();
+        }
+
+        public static int SquareEachDigit(int n)
+        {
+            string OutPut = "";
+            string digits = n.ToString();
+
+            foreach (char digit in digits)
+            {
+                int number = int.Parse(digit.ToString());
+                OutPut += Math.Pow(number, 2);
+            }
+
+            n = int.Parse(OutPut);
+
+            return n;
         }
     }
 }
